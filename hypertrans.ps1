6a305614993697ae9ceaa333afa5077ee8e6e6b7
Jongwon <lastone9182@gmail.com>
2017-03-16 17:10:45 +0900

    context deactivate

--- hypertrans.ps1
+++ hypertrans.ps1
@@ -1,2 +1,11 @@
+# Windows Powershell Script
+# > .\hypertrans.ps1
+
+# virtualenv activate
 .\_hypertrans\Scripts\activate.ps1
+
+# module loop
 python main.py
+
+# virtualenv deactivate
+deactivate
